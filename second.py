5851e0e076ab2f7d4a5c4f5a6e3ca3192d46741a
Akshaya <akshayasekharan21@gmail.com>
2024-02-09 20:38:14 +0530

    third commit

--- second.py
+++ second.py
@@ -8,14 +8,21 @@ def hexagon():
     print(space, end="")
     first = underscore + 5 * space
     second = slash + 3 * space + backslash + underscore
-    third = backslash + underscore + slash + 3 * space
-
-    print(row * first)
-
-    for i in range(1, row + 1):
-        print((row-1) * second, end="")
-        print(slash + 3 * space + backslash)
-        print(row * third)
-
+    third = backslash + underscore + slash + 3 * space 
+    col1 = column % 2
+    col2 = int(column / 2)
+    col = col1 + col2
+    print(col * first)
+    if col1 == 0:
+        for i in range(1, row + 1):
+           print((col) * second, end="")
+           print(slash)
+           print((col) * third, end="")
+           print(backslash)
+    else: 
+        for i in range(1, row + 1):
+           print((col-1) * second, end="")
+           print(slash + 3 * space + backslash)
+           print(col * third)
 
 hexagon()
